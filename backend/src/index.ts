d008e83d7893bf3a8961ee142912a05966b8c79c
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-06-03 01:24:54 +0530

    Some extra

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,17 +2,23 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute" 
+import myUserRoute from "./routes/MyUserRoute";
 
-mongoose.connect(process.env.MONGODB_URI as string).then(() => console.log("Connected to DB"));
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log("Connected to DB"));
 
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Health Ok!!!" });
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(5000, () => {
   console.log("Server started on localhost: 5000");
-})
+});
