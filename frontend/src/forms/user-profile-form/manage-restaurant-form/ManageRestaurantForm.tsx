1a3d38af05cdb30c275870f0fbd27278dbc610c0
Joyjeet Mukherjee <joyxbhp6@gmail.com>
2024-06-30 01:12:05 +0530

    Cuisines added

--- frontend/src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -52,15 +54,20 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
 
   const onSubmit = (FormDataJson: restaurantFormData) => {
     // Todo - convert formDataJson to formDataObject
-  }
+  };
 
-  return(
+  return (
     <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
-            <DetailsSection />
-        </form>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+        <Separator />
+        <CuisinesSection />
+      </form>
     </Form>
-  )
+  );
 };
 
 export default ManageRestaurantForm;
